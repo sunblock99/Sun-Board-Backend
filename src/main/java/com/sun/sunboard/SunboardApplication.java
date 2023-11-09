cd4e92bd5ac1f1290426123cb9c25f150e86ccff
sunblock99 <sunblock99@kakao.com>
2023-11-09 17:10:08 +0900

    springboot init

---
+++ src/main/java/com/sun/sunboard/SunboardApplication.java
@@ -0,0 +1,13 @@
+package com.sun.sunboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sun/sunboard/SunboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.sunboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
