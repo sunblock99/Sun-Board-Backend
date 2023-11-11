53c42f5faf25fa6bd1ea5fd9e8a48a7393af67de
sunblock99 <sunblock99@kakao.com>
2023-11-11 17:46:05 +0900

    [#1] 회원가입기능 구현

--- src/main/java/com/sun/sunboard/controller/UserController.java
+++ src/main/java/com/sun/sunboard/controller/UserController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-
-
     private final UserService userService;
 
     /**
