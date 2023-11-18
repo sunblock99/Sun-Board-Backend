575fea4b823d9b15ed3096c3c12747521b706ec1
sunblock99 <sunblock99@kakao.com>
2023-11-18 09:41:17 +0900

    [#7] 세션 만료 이셉션 추가

---
+++ src/main/java/com/sun/sunboard/error/exception/NullSessionException.java
@@ -0,0 +1,7 @@
+package com.sun.sunboard.error.exception;
+
+public class NullSessionException extends RuntimeException{
+    public NullSessionException(String msg) {
+        super(msg);
+    }
+}
